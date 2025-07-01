67d95e5c2da3a954e866228e54005a17dc27ad22
yandimant1993 <yan1993dimant@gmail.com>
2025-07-01 19:10:28 +0300

    Limit ball size

--- main.js
+++ main.js
@@ -7,4 +7,14 @@ function onBallClick(clickedEl) {
     clickedEl.style.width = gSize + 'px'
     clickedEl.style.height = gSize + 'px'
     clickedEl.innerText = gSize
+    if (gSize >= 400) {
+        limitBallSize(clickedEl)
+    }
+}
+
+function limitBallSize(clickedEl) {
+    gSize = 100
+    clickedEl.style.width = gSize + 'px'
+    clickedEl.style.height = gSize + 'px'
+    clickedEl.innerText = gSize
 }
