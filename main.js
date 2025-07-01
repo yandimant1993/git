d7a8ad5c3d267fd5f4893a23d87106f21d9e1dbc
yandimant1993 <yan1993dimant@gmail.com>
2025-07-01 20:19:39 +0300

    Randomly change ball color

--- main.js
+++ main.js
@@ -3,19 +3,21 @@
 var gSize = getRandomInt(20, 61)
 
 function onBallClick(clickedEl) {
+    var color = getRandomColor()
     // gSize += 50
+    clickedEl.style.backgroundColor = color
     gSize = getRandomInt(20, 61)
     clickedEl.style.width = gSize + 'px'
     clickedEl.style.height = gSize + 'px'
     clickedEl.innerText = gSize
-    if (gSize >= 400) {
-        limitBallSize(clickedEl)
-    }
+    // if (gSize >= 400) {
+    //     limitBallSize(clickedEl)
+    // }
 }
 
-function limitBallSize(clickedEl) {
-    gSize = getRandomInt(20, 61)
-    clickedEl.style.width = gSize + 'px'
-    clickedEl.style.height = gSize + 'px'
-    clickedEl.innerText = gSize
-}
+// function limitBallSize(clickedEl) {
+//     gSize = getRandomInt(20, 61)
+//     clickedEl.style.width = gSize + 'px'
+//     clickedEl.style.height = gSize + 'px'
+//     clickedEl.innerText = gSize
+// }
--- util.js
+++ util.js
@@ -1,13 +1,13 @@
 'use strict'
 
-// function getRandomColor() {
-//     var letters = '0123456789ABCDEF';
-//     var color = '#';
-//     for (var i = 0; i < 6; i++) {
-//         color += letters[Math.floor(Math.random() * 16)];
-//     }
-//     return color;
-// }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
